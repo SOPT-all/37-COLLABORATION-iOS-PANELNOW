87670326b025ff756191e7a68fb4339d4273bffd
hyeyeonie <shy020916@naver.com>
2025-11-17 19:53:52 +0900

    [add] 초기 세팅 및 라이브러리 추가

---
+++ PANELNOW/PANELNOW/Presentation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PANELNOW
+//
+//  Created by 신혜연 on 11/17/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
