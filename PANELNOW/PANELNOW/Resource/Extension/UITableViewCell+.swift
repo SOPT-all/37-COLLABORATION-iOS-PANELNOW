d5861bfd58ae4ba7b0c737f1df2924ba10430f9e
KuKaH <kukah99@gmail.com>
2025-11-20 14:43:20 +0900

    [add] UITableViewCell extension 추가

---
+++ PANELNOW/PANELNOW/Resource/Extension/UITableViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+.swift
+//  PANELNOW
+//
+//  Created by 홍준범 on 11/20/25.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier : String {
+        return String(describing: self)
+    }
+}
